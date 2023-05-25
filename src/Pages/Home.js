3e83a4b93bf57ea33812af87669d571eb147a597
adityaiiitr <adityaiiitr@gmail.com>
2023-05-25 19:07:48 +0530

    Home Page Edited!

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,22 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/form');
+  };
   return (
-    <div>Home <Link to={'/form'}>Form</Link></div>
+    <>
+    <div className='h-screen flex items-center justify-center mt-24 h-1/2'>
+    <button className='bg-opacity-60 text-white bg-gray-600 hover:bg-black focus:ring-gray-800 dark:focus:ring-white-200 font-medium rounded-lg text-sm  h-60 w-60' onClick={handleClick}>Click Here</button>
+    </div>
+    </>
+      
+      
+
+    
   )
 }
 
